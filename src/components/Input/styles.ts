f0bb43be489a4917d3039612185483dc4bd34c9f
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-08-23 22:08:45 -0300

    improve input usability

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,17 +1,35 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   border-radius: 8px;
-  border: 1px solid #717171;
   padding: 16px;
   width: 100%;
   display: flex;
   align-items: center;
+
+  border: 2px solid #717171;
   color: #717171;
 
   & + div {
     margin-top: 8px;
   }
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #0748b4;
+      border-color: #0748b4;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #0748b4;
+    `}
 
   input {
     flex: 1;
