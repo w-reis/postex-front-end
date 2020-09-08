83dba0d8fe3bea283581e0d5d2024f8b746e7916
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-09-07 21:40:50 -0300

    create function to remove toast

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -11,7 +11,7 @@ export interface ToastMessage {
 
 interface ToastContextData {
   addToast(message: Omit<ToastMessage, 'id'>): void;
-  removeToast(): void;
+  removeToast(id: string): void;
 }
 
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
@@ -34,8 +34,8 @@ export const ToastProvider: React.FC = ({ children }) => {
     [],
   );
 
-  const removeToast = useCallback(() => {
-    console.log('removeToast');
+  const removeToast = useCallback((id: string) => {
+    setMessages((state) => state.filter((message) => message.id !== id));
   }, []);
   return (
     <ToastContext.Provider value={{ addToast, removeToast }}>
