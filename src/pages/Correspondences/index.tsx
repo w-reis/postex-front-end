0bb6002b3c4bd5ab0078c3b3f4d8f51903e4b076
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-09-09 23:21:18 -0300

    create correspondences page

---
+++ src/pages/Correspondences/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Correspondences: React.FC = () => {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  );
+};
+
+export default Correspondences;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
+import Correspondences from '../pages/Correspondences';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+      <Route path="/correspondences" component={Correspondences} />
     </Switch>
   );
 };
