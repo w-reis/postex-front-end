1194b7e73c8e8a46a0849c3c7548d3412c31ca59
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-09-12 16:46:09 -0300

    create responsive dafault layout

--- src/pages/layouts/default/styles.ts
+++ src/pages/layouts/default/styles.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  @media (min-width: 992px) {
+    width: 100vw;
+    display: grid;
+    grid-template-columns: 244px 1fr;
+    grid-template-rows: 60px 1fr;
+    grid-template-areas:
+      'sidebar header'
+      'sidebar .';
+    min-height: 100%;
+  }
+`;
