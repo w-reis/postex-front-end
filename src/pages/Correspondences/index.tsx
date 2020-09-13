5d1e12d26552133bbde003450436a5fc18e5eca9
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-09-13 19:00:55 -0300

    create correspondence list

--- src/pages/Correspondences/index.tsx
+++ src/pages/Correspondences/index.tsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { GoSearch } from 'react-icons/go';
+
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+
 import { Container } from './styles';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 const Correspondences: React.FC = () => {
+  const cartas = [
+    {
+      id: 1,
+      name: 'Wellinton Fabricio dos Reis',
+      number: '549843839274',
+      status: 'pendente',
+    },
+
+    {
+      id: 2,
+      name: 'Alvaro Valdivino dos Reis',
+      number: '549843839274',
+      status: 'pendente',
+    },
+  ];
+  const formRef = useRef<FormHandles>(null);
   return (
     <Container>
-      <h1>Hello World!</h1>
+      <div>
+        <Form onSubmit={() => {}} ref={formRef}>
+          <Input name="query" icon={GoSearch} placeholder="Buscar" autoFocus />
+        </Form>
+
+        <Button type="button">Novo</Button>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th> </th>
+            <th>ID</th>
+            <th>Destinatário</th>
+            <th>Nº do objeto</th>
+            <th>Status</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartas.map((carta) => (
+            <tr key={carta.id}>
+              <td>
+                <label htmlFor="check">
+                  <input type="checkbox" id="check" />
+                </label>
+              </td>
+              <td>{carta.id}</td>
+              <td>{carta.name}</td>
+              <td>{carta.number}</td>
+              <td>{carta.status}</td>
+              <td>editar | excluir | entregar</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Container>
   );
 };
--- src/pages/Correspondences/styles.ts
+++ src/pages/Correspondences/styles.ts
@@ -1,3 +1,94 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 95%;
+  margin: 47px auto;
+  align-items: center;
+
+  & > div {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: space-between;
+    align-items: flex-start;
+
+    form {
+      width: 100%;
+      max-width: 392px;
+      margin-bottom: 16px;
+
+      & > div {
+        padding: 11px;
+      }
+    }
+
+    button {
+      height: 47px;
+      padding: 11px;
+      align-content: flex-end;
+      transition: all 0.5s ease;
+      width: 100px;
+    }
+
+    button:hover {
+      background-color: rgb(4, 54, 136);
+      color: #ffffff;
+    }
+
+    @media (min-width: 994px) {
+      form {
+        align-self: flex-start;
+      }
+    }
+  }
+
+  table {
+    width: 100%;
+    margin-bottom: 20px;
+    border-collapse: collapse;
+
+    thead th {
+      background: #eeeeee;
+      vertical-align: bottom;
+      font-weight: 600;
+    }
+
+    tbody > tr:nth-child(even) > td,
+    tbody > tr:nth-child(even) > th {
+      background-color: #ffffff;
+    }
+
+    th,
+    td {
+      font-weight: normal;
+      font-size: 12px;
+      padding: 8px 15px;
+      line-height: 20px;
+      text-align: left;
+      vertical-align: middle;
+      border-top: 1px solid #cccccc;
+    }
+
+    td:nth-child(-n + 2) {
+      width: 5%;
+    }
+  }
+
+  @media (max-width: 994px) {
+    table thead {
+      display: none;
+    }
+    table tr {
+      border-bottom: 1px solid #dddddd;
+    }
+    table td {
+      border: 0;
+    }
+    table td:not(:first-child) {
+      display: block;
+    }
+  }
+`;
