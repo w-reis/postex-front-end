e2964c8634ea8ff2dbfed5c7fb2ff35e6a8a4ea9
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-10-05 16:22:12 -0300

    changes pagination style

--- src/components/Pagination/styles.ts
+++ src/components/Pagination/styles.ts
@@ -13,15 +13,26 @@ export const Container = styled.div`
       min-width: 40px;
       height: 40px;
       padding: 4px;
-      border-radius: 4px;
+      border: none;
       border: 1px solid #0949b4;
+      border-left: none;
       font-size: 12px;
       text-decoration: none;
-
+      border-collapse: collapse;
       transition: all 0.5s ease;
-      margin-right: 8px;
       color: #0949b4;
       font-size: 20px;
+    }
+
+    button:first-child {
+      border-left: 1px solid #0949b4;
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+    }
+
+    button:last-child {
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
     }
 
     button:hover {
